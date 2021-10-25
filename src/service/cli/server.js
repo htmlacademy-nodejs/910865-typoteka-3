3fa527d1b55bb67147ba2803aa8d2659347d28d0
Ozkhan Mamedov <ozhanm96@yandex.ru>
2021-10-25 16:33:43 +0300

    рефакторинг с использованием express

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,50 +1,27 @@
 'use strict';
 
-const http = require(`http`);
+const express = require(`express`);
 const chalk = require(`chalk`);
 const fs = require(`fs`).promises;
 
 const {DEFAULT_PORT, FILE_NAME, HttpCode, NOT_FOUND_ERROR_MESSAGE} = require(`../../constants`);
 
-const sendResponse = (res, statusCode, msg) => {
-  const template = `
-  <!Doctype html>
-    <html lang="ru">
-      <head>
-        <title>Typoteka</title>
-      </head>
-      <body>${msg}</body>
-    </html>
-  `.trim();
+const app = express();
 
-  res.writeHead(statusCode, {
-    'Content-Type': `text/html; charset=UTF-8`,
-  });
+app.use(express.json());
+app.get(`/posts`, async (req, res) => {
+  try {
+    const fileContent = await fs.readFile(FILE_NAME);
+    const mocks = JSON.parse(fileContent);
 
-  res.end(template);
-};
-
-const onClientConnect = async (req, res) => {
-  switch (req.url) {
-    case `/`:
-      try {
-        const content = await fs.readFile(FILE_NAME);
-        const mocks = JSON.parse(content);
-        const message = mocks.map((it) => `<li>${it.title}</li>`).join(``);
-
-        sendResponse(res, HttpCode.OK, `<ul>${message}</ul>`);
-      } catch (err) {
-        sendResponse(res, HttpCode.NOT_FOUND, NOT_FOUND_ERROR_MESSAGE);
-      }
-
-      break;
-
-    default:
-      sendResponse(res, HttpCode.NOT_FOUND, NOT_FOUND_ERROR_MESSAGE);
-
-      break;
+    res.json(mocks);
+  } catch (err) {
+    res.send([]);
   }
-};
+});
+app.use((req, res) => res
+  .status(HttpCode.NOT_FOUND)
+  .send(NOT_FOUND_ERROR_MESSAGE));
 
 module.exports = {
   name: `--server`,
@@ -52,13 +29,12 @@ module.exports = {
     const [port] = args;
     const userPort = Number.parseInt(port, 10) || DEFAULT_PORT;
 
-    http.createServer(onClientConnect)
-      .listen(userPort)
-      .on(`listening`, () => {
+    app.listen(userPort, (err) => {
+      if (err) {
+        console.info(chalk.red(`Ошибка при создании сервера: ${err}`));
+      } else {
         console.info(chalk.green(`Ожидаю соединений на ${userPort}`));
-      })
-      .on(`error`, ({message}) => {
-        console.info(chalk.red(`Ошибка при создании сервера: ${message}`));
-      });
+      }
+    });
   }
 };
