2746ab6d804ee418c96674b6f01faf7a8891b7ad
Ozkhan Mamedov <ozhanm96@yandex.ru>
2021-11-09 02:48:41 +0300

    подключил логгер в server.js

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,13 +1,14 @@
 'use strict';
 
 const express = require(`express`);
-const chalk = require(`chalk`);
 
 const routes = require(`../api`);
+const {getLogger} = require(`../lib/logger`);
 
-const {DEFAULT_PORT, HttpCode, NOT_FOUND_ERROR_MESSAGE} = require(`../../constants`);
+const {DEFAULT_PORT, HttpCode, NOT_FOUND_ERROR_MESSAGE, ExitCode} = require(`../../constants`);
 
 const app = express();
+const logger = getLogger({name: `api`});
 
 app.use(express.json());
 app.use(`/api`, routes);
@@ -21,12 +22,17 @@ module.exports = {
     const [port] = args;
     const userPort = Number.parseInt(port, 10) || DEFAULT_PORT;
 
-    app.listen(userPort, (err) => {
-      if (err) {
-        console.info(chalk.red(`Ошибка при создании сервера: ${err}`));
-      } else {
-        console.info(chalk.green(`Ожидаю соединений на ${userPort}`));
-      }
-    });
+    try {
+      app.listen(userPort, (err) => {
+        if (err) {
+          return logger.error(`An error occurred on server creation: ${err.message}`);
+        }
+
+        return logger.info(`Listening to connections on ${userPort}`);
+      });
+    } catch (err) {
+      logger.error(`An error occurred: ${err.message}`);
+      process.exit(ExitCode.error)
+    }
   }
 };
