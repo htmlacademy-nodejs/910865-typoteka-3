cbfedbc1a9fee79251b2578aaedcacd8840a3e4d
Ozkhan Mamedov <ozhanm96@yandex.ru>
2021-11-09 20:17:56 +0300

    убрал передачу лишней переменной окружения в package.json

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -12,6 +12,13 @@ const logger = getLogger({name: `api`});
 
 app.use(express.json());
 app.use(`/api`, routes);
+app.use((req, res, next) => {
+  logger.debug(`Request on route ${req.url}`);
+  res.on(`finish`, () => {
+    logger.info(`Response status code ${res.statusCode}`);
+  });
+  next();
+});
 app.use((req, res) => {
   res.status(HttpCode.NOT_FOUND)
     .send(NOT_FOUND_ERROR_MESSAGE);
@@ -19,13 +26,6 @@ app.use((req, res) => {
 });
 app.use((err, _req, _res, _next) => {
   logger.error(`An error occurred on processing request: ${err.message}`);
-});
-app.use((req, res, next) => {
-  logger.debug(`Request on route ${req.url}`);
-  res.on(`finish`, () => {
-    logger.info(`Response status code ${res.statusCode}`);
-  });
-  next();
 });
 
 module.exports = {
