04fbde175ca946cd9fda34095ff5f24f63b30104
Ozkhan Mamedov <ozhanm96@yandex.ru>
2021-11-09 03:07:31 +0300

    расширил логирование server.js

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -12,9 +12,21 @@ const logger = getLogger({name: `api`});
 
 app.use(express.json());
 app.use(`/api`, routes);
-app.use((req, res) => res
-  .status(HttpCode.NOT_FOUND)
-  .send(NOT_FOUND_ERROR_MESSAGE));
+app.use((req, res) => {
+  res.status(HttpCode.NOT_FOUND)
+    .send(NOT_FOUND_ERROR_MESSAGE);
+  logger.error(`Route not found: ${req.url}`);
+});
+app.use((err, _req, _res, _next) => {
+  logger.error(`An error occurred on processing request: ${err.message}`);
+});
+app.use((req, res, next) => {
+  logger.debug(`Request on route ${req.url}`);
+  res.on(`finish`, () => {
+    logger.info(`Response status code ${res.statusCode}`);
+  });
+  next();
+});
 
 module.exports = {
   name: `--server`,
