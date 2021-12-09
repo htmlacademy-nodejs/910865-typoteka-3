edd3da0005eb60e7fb701bd0304757d92dac2bd5
Ozkhan Mamedov <ozhanm96@yandex.ru>
2021-12-09 18:25:55 +0300

    правки по замечаниям

--- src/service/lib/sequelize.js
+++ src/service/lib/sequelize.js
@@ -4,7 +4,7 @@ const Sequelize = require(`sequelize`);
 
 const {DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT} = process.env;
 
-const somethingIsNotDefined = [DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT].some((it) => it === undefined);
+const somethingIsNotDefined = [DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT].some((it) => typeof it === 'undefined');
 
 if (somethingIsNotDefined) {
     throw new Error(`One or more environmental variables are not defined`);
